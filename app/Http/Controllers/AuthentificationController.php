a01471f1cbfcc5aa6f5d9122893fef5e6c844c73
Monica <monica.walentek@gmail.com>
2022-06-19 14:50:02 +0200

    authentification finished

--- app/Http/Controllers/AuthentificationController.php
+++ app/Http/Controllers/AuthentificationController.php
@@ -9,50 +9,47 @@ use Symfony\Component\HttpFoundation\Response;
 class AuthentificationController extends Controller
 {
     public function createCustomer(Request $request)
-   {
+    {
 
-    $data = $request->all();
+        $data = $request->all();
 
-    return Customers::create([
-        'last_name' => $data['lastName'],
-        'first_name' => $data['firstName'],
-        'email' => $data['email'],
-        'password' => $data['password'], // the password is not hashed :)
-        'roles_id' => '1',
-        'phone' => (string) $data['phone'] ?? null,
-        'gender' => $data['gender'],
-        'status' => 'actif',
-        'birthdate' => $data['birthdate']
-    ]);
-   }
+        return Customers::create([
+            'last_name' => $data['lastName'],
+            'first_name' => $data['firstName'],
+            'email' => $data['email'],
+            'password' => $data['password'], // the password is not hashed :)
+            'phone' => (string) $data['phone'] ?? null,
+            'gender' => $data['gender'],
+            'status' => 'actif',
+            'birthdate' => $data['birthdate']
+        ]);
 
-   public function getCustomer(Request $request)
-   {
-    
-    $id = $request['id'];
-    $customer = Customers::where('id', $request['id'])->get();
-    return [
-        'customer' => $customer
-    ];
-   }
-    
-
-   /* public function login(Request $request) 
-   {
-    if(!auth()->guard('customers')->attempt([
-        'email' => $request->email,
-        'password' => $request->password,
-    ])) {
-        return response([
-            'message' => 'Invalid credentials.'
-        ], Response::HTTP_UNAUTHORIZED); 
+        // I think we don't need to catch error and return a message in case of issue
     }
-        $user = auth()->guard('customers')->user();
-        $token = $user->createToken('token')->plainTextToken; // we will store the token in the cookies
-        $cookie = cookie('jwt', $token, 60*24); // token for 1 day
-        return response([
-            'message' => 'Success.',
-            'token' => $token
-        ], Response::HTTP_ACCEPTED)->withCookie($cookie);
-   } */
+
+    public function getCustomer(Request $request)
+    {
+
+        $id = $request['id'];
+        $customer = Customers::where('id', $request['id'])->get();
+        return [
+            'customer' => $customer
+        ];
+    }
+
+
+    public function login(Request $request)
+    {
+        $resultEmail = Customers::where('email', $request['email'])->value('password'); // I think it's the best way to check the password :)
+        if ($resultEmail == []) {
+            return "L'email n'est pas dans la base de données";
+        } else {
+            if ($resultEmail == $request['password']) {
+                // return Customers::where('email', $request['email'])->get(); --> Instead of returning a token, why can't we just return everything ? I think it will save the number of request, really...
+                return Customers::where('email', $request['email'])->get('id');
+            } else {
+                return 'Ce n\'est pas le bon mot de passe, mais l\'email existe.';
+            }
+        }
+    }
 }
